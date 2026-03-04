e7fd2e085d342e3b12ac4d1187440bd8ec065555
faridavila <farid.avila01@uceva.edu.co>
2026-03-04 18:05:01 -0500

    cierre de sesion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// App.tsx
 import { useState, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { requestFirebaseNotificationPermission } from './firebase';
